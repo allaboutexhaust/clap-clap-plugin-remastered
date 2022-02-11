d77dbc3157926760f5ece7d75018e80202c37e66
cuttyypl <picadoder67@outlook.com>
2022-02-11 15:15:10 +0100

    Create clap.plugin.js
    First and propably final commit OwO

---
+++ clap.plugin.js
@@ -0,0 +1,143 @@
+/**
+ * @name ClapClapPluginRemastered
+ * @invite amethystclient
+ * @authorLink https://github.com/devpedzio
+ * @website https://whores.best
+ */
+
+module.exports = (() =>
+{
+    const config =
+    {
+		info:
+		{
+			name: "ClapClapPluginRemastered",
+			authors:
+			[
+				{
+					name: "pedzio",
+					discord_id: "894433798836613160",
+					github_username: "devpedzio",
+					twitter_username: "devpedzio"
+				}
+			],
+			version: "1.0.0",
+			description: "Useless plugin. Type c? <message> to clap",
+			github: "",
+			github_raw: ""
+		},
+		changelog:
+		[
+			{
+				title: "1.0.0",
+				type: "added",
+				items:
+				[
+					"Added 'cw?' option. This will capitalize every other letter in your message, showing that you are, in fact, the most intelligent and dominant person in chat."
+				]
+			}
+		]
+    };
+
+    return (([Plugin, Api]) => {
+
+		const plugin = (Plugin, Api) =>
+		{
+			const { DiscordModules, Patcher } = Api;
+
+			return class ClapClapPluginRemastered extends Plugin
+			{
+				constructor()
+				{
+					super();
+				}
+	
+				onStart()
+				{
+					Patcher.after(DiscordModules.MessageActions, "sendMessage", (_, [, message]) =>
+					{
+						const content = message.content.toLowerCase();
+						const clapclap = (/^c(\S| )*\?/g).exec(content) || (/^sc(\S| )*\?/g).exec(content);
+						
+						if (clapclap)
+						{	
+							const filler = clapclap[0].includes("(") && clapclap[0].includes(")")
+								? clapclap[0].substr(clapclap[0].indexOf("(") + 1, clapclap[0].indexOf(")") - clapclap[0].indexOf("(") - 1)
+								: " :clap: ";
+								
+							message.content = message.content.substr(clapclap[0].length, message.content.length)
+								.split(clapclap[0].startsWith("super") ? "" : " ")
+								.join(filler);
+								
+							message.content = filler + message.content + filler;
+
+							return;
+						}
+
+						switch (content.split("$")[0])
+						{
+							case "ra":
+								const ra = (/^ra\?/g).exec(content);
+
+								message.content = message.content.toLowerCase().substr(ra[0].length, message.content.length)
+									.split(" ")
+									.join("\t")
+									.replace(/[A-Za-z]/g, x => ` :regional_indicator_${x.toLowerCase()}: `);
+								
+								break;
+
+							case "reverse":
+								const reverse = (/^rev\?/g).exec(content);
+
+								message.content = message.content.substr(reverse[0].length, message.content.length)
+									.split("")
+									.reverse()
+									.join("");
+
+								break;
+
+							case "owo":
+								const owo = (/^owo\?/g).exec(content);
+
+								message.content = message.content.substr(owo[0].length, message.content.length)
+									.replace(/r/g, "w")
+									.replace(/R/g, "W")
+									.replace(/l/g, "w")
+									.replace(/L/g, "W")
+									.replace(/ n/g, " ny")
+									.replace(/ N/g, " Ny")
+									.replace(/ove/g, "uv")
+									.replace(/OVE/g, "UV")
+									+ " " + ["owo", "OwO", "uwu", "UwU", ">w<", "^w^", "♥w♥"][7 * Math.random() << 0];
+
+								break;
+
+							case "b":
+								const b = (/^b\?/g).exec(content);
+
+								message.content = message.content.substr(b[0].length, message.content.length)
+									.replace(/b/g, ":b:");
+
+								break;
+							
+							case "woke":
+								const woke = (/^cw\?/g).exec(content);
+
+								message.content = message.content.substr(woke[0].length, message.content.length)
+									.replace(/.{2}/gm, c => c[0].toUpperCase() + c[1].toLowerCase());
+
+								break;
+						}
+					});
+				}
+	
+				onStop()
+				{
+					Patcher.unpatchAll();
+				}
+			}
+		};
+
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
